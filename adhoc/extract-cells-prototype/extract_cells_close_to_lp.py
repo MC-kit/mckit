22eb717fb65862cf6107919f5f8ea9c6465df9f9
dvp <d.portnov@iterrf.ru>
2026-06-26 18:41:01 +0300

    feat: add protyping code for cell extraction

---
+++ adhoc/extract-cells-prototype/extract_cells_close_to_lp.py
@@ -0,0 +1,118 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Extract cells close to lost particle point
+
+# %%
+from typing import Iterable
+
+# %%
+import sys
+
+from pathlib import Path
+
+# %%
+print("Python", sys.version, "at", sys.prefix)
+
+# %%
+from mckit import Body, Universe
+from mckit.body import Card
+from mckit.parser import from_file
+
+# %%
+# !ls
+
+# %%
+Path.cwd()
+
+# %%
+cell_numbers_to_select = [187246]
+
+# %%
+# !ls
+
+# %%
+original_model_path = Path("pc11-1.1/pc11.i")
+assert original_model_path.exists()
+
+# %%
+original_model = from_file(original_model_path)
+
+# %%
+original_universe = original_model.universe
+
+# %%
+len(original_universe)
+
+# %%
+selected_cells: list[Body] = [c for c in original_universe if c.name() in cell_numbers_to_select]
+
+# %%
+len(selected_cells)
+
+# %%
+selected_surfaces = set()
+for c in selected_cells:
+    selected_surfaces.update(c._shape.get_surfaces())
+
+# %%
+len(selected_surfaces)
+
+# %%
+adjacent_cells: list[Body] = [c for c in original_universe if c.name() not in cell_numbers_to_select and c._shape.get_surfaces() & selected_surfaces]
+
+# %%
+len(adjacent_cells)
+
+
+# %%
+def list_names(cards: list[Card]) -> Iterable[int]:
+    return (c.name() for c in cards)
+
+
+# %%
+sorted(list_names(adjacent_cells))
+
+# %%
+new_cells = sorted(selected_cells + adjacent_cells, key=Body.name)
+
+# %%
+list(list_names(new_cells))
+
+# %%
+len(new_cells)
+
+# %%
+selected_universe = Universe(new_cells)
+
+# %%
+selected_universe.save("pc11-1.1/lp/selected.i")
+
+# %%
+# !ls pc11-1.1/lp
+
+# %%
+sorted(list_names(selected_surfaces))
+
+# %%
+clashed_surface_numbers: set[int] = set([201392])
+
+# %%
+cells_with_clashed_surfaces: list[Body] = [c for c in original_universe if set(s.name() for s in c._shape.get_surfaces()) & clashed_surface_numbers]
+
+# %%
+sorted(list_names(cells_with_clashed_surfaces))
+
+# %%
