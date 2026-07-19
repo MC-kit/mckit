84e1f21883d912817b2627cd255467c7443d32a9
dvp <d.portnov@iterrf.ru>
2026-07-19 11:47:05 +0300

    docs(notebook): shift TRT зала

--- notebooks/dvp/shift_trt_building.py
+++ notebooks/dvp/shift_trt_building.py
@@ -1,14 +1,16 @@
 import marimo
 
-import mckit.utils._path
-
 __generated_with = "0.23.14"
 app = marimo.App(width="medium")
 
 with app.setup:
+    from typing import Iterable
+
     import sys
 
     from pathlib import Path
+
+    import numpy as np
 
 
 @app.cell
@@ -50,14 +52,14 @@ def _(mc):
 
 @app.cell
 def _(ROOT):
-    VERSION="0.1.0"
+    VERSION="0.1.1"
     OUT=ROOT / f".wrk/{Path(__file__).stem}/{VERSION}"
     return (OUT,)
 
 
 @app.cell
 def _(OUT, mc):
-    mckit.utils._path.make_dir(OUT)
+    mc.utils._path.make_dir(OUT)
     return
 
 
@@ -75,7 +77,54 @@ def _(INPUT_PATH, mc):
 
 @app.cell
 def _(building_info):
-    tr1 = building_info.transformations[0]
+    building = building_info.universe
+    return (building,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    surface33 - поверхность пола в зале
+    """)
+    return
+
+
+@app.cell
+def _(building_info):
+    surface33 = building_info.surfaces_index[33]
+    return (surface33,)
+
+
+@app.cell
+def _(surface33):
+    surface33
+    return
+
+
+@app.cell
+def _(surface33):
+    surface33.mcnp_repr()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Смотри замету в Zotero "Положение токамака ТРТ в здании" о связи систем координат токамака и здания.
+    После смещения здания поверхность 33 должна стать PZ -583.5, чтобы опоры установки "почти касались" пола.
+    """)
+    return
+
+
+@app.cell
+def _():
+    translation=np.array([-29, 1556, -589], dtype=np.float64)
+    return (translation,)
+
+
+@app.cell
+def _(mc, translation):
+    tr1 = mc.Transformation(translation=translation)
     return (tr1,)
 
 
@@ -86,32 +135,51 @@ def _(tr1):
 
 
 @app.cell
-def _(tr1):
-    tr_reversed = tr1.reverse()
-    return (tr_reversed,)
+def _(building, tr1):
+    building_shifted = building.transform(tr1)
+    return (building_shifted,)
 
 
 @app.cell
-def _(tr1, tr_reversed):
-    tr_reversed.apply2transform(tr1).apply2point([0,0,0])
+def _(building_shifted):
+    new_surfaces = building_shifted.get_surfaces()
+    return (new_surfaces,)
+
+
+@app.function
+def find_surface(name: int, surfaces):
+    return next((x for x in surfaces if x.name() == name), None)
+
+
+@app.cell
+def _(new_surfaces):
+    new_surface33 = find_surface(33, new_surfaces)
+    return (new_surface33,)
+
+
+@app.cell
+def _(new_surface33):
+    new_surface33.mcnp_repr()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Поверхность 33 там, где нужно.
+    """)
     return
 
 
 @app.cell
-def _(building_info, tr_reversed):
-    new_universe = building_info.universe.transform(tr_reversed)
-    return (new_universe,)
-
-
-@app.cell
-def _(new_universe):
-    new_universe._verbose_name = "hall-1.2.3-shifted"
+def _(building_shifted):
+    building_shifted._verbose_name = "hall-1.2.3-shifted"
     return
 
 
 @app.cell
-def _(OUT, new_universe):
-    new_universe.save(OUT / "hall_shifted.mcnp")
+def _(OUT, building_shifted):
+    building_shifted.save(OUT / "hall_shifted.mcnp")
     return
 
 
