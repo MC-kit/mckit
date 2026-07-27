83078a0abb5f766b547e8e4ea09e1ae844f302da
dvp <d.portnov@iterrf.ru>
2026-07-27 16:02:33 +0300

    dependencies: update @amarano

--- src/mckit/body.py
+++ src/mckit/body.py
@@ -40,7 +40,7 @@ if TYPE_CHECKING:
     type TGeometry = list[Surface | ShapeOperationCode | Shape | Body] | Body | Shape
 
 
-__all__ = ["GLOBAL_BOX", "Body", "Card", "Shape", "TGeometry", "TGeometry", "simplify"]
+__all__ = ["GLOBAL_BOX", "Body", "Card", "Shape", "TGeometry", "simplify"]
 
 _LOG = getLogger(__name__)
 
