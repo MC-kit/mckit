2def44998b7e68839f290c2879d40489931a1009
dvp <d.portnov@iterrf.ru>
2026-09-02 14:38:34 +0300

    build: relax ClangTidy script

--- cmake/module/ClangTidy.cmake
+++ cmake/module/ClangTidy.cmake
@@ -1,8 +1,12 @@
 function(AddClangTidy target)
-    find_program(CLANG-TIDY_PATH clang-tidy REQUIRED)
-    set_target_properties(${target}
-        PROPERTIES CXX_CLANG_TIDY
-        # "${CLANG-TIDY_PATH};-checks=*;--warnings-as-errors=*"
-        "${CLANG-TIDY_PATH};-checks=*"
-    )
+    find_program(CLANG_TIDY_PATH clang-tidy)
+    if (CLANG_TIDY_PATH)
+        set_target_properties(${target}
+            PROPERTIES CXX_CLANG_TIDY
+            # "${CLANG_TIDY_PATH};-checks=*;--warnings-as-errors=*"
+            "${CLANG_TIDY_PATH}"
+        )
+    else()
+        message(WARNING "clang-tidy not found")
+    endif()
 endfunction()
